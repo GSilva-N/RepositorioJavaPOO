89c8430c1e72e7bb67eb9c2570a1f879a126179e
Guilherme <guilherme.paganini10@gmail.com>
2020-09-15 22:39:35 -0300

    POO

--- Lista01 - Classes e Objetos/src/Questoes/FuncionarioObjeto.java
+++ Lista01 - Classes e Objetos/src/Questoes/FuncionarioObjeto.java
@@ -4,8 +4,6 @@ public class FuncionarioObjeto {
 
 	public static void main(String[] args) {
 		
-		Scanner kb = new Scanner(System.in);
-		
 		FuncionarioClasse funcionario01 = new FuncionarioClasse("Marcelo Santos", 1005, "Desenvolvedor");
 		
 		funcionario01.dadosFuncionario();
